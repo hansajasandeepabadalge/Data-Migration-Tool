ff49e7714f0754fd29b0169e4c72212bbd712adb
Hansaja Sandeepa <76199476+hansajasandeepabadalge@users.noreply.github.com>
2025-04-29 11:34:20 +0530

    Add dynamic file upload and display functionality
    Implemented dynamic file handling, including upload, display, and removal functionalities. Enhanced user experience with "Add more files" and "Remove all files" actions. Updated styles and interaction logic for improved usability.

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+
+interface UploadedFile {
+  id: number;
+  name: string;
+  size: string;
+}
+
+
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
+
 export class MainComponent {
+
+  uploadedFiles: UploadedFile[] = [];
+
+  handleFileUpload(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files) {
+      const files = Array.from(input.files);
+
+      files.forEach(file => {
+        const sizeInKB = (file.size / 1024).toFixed(2);
+
+        const fileInfo: UploadedFile = {
+          id: Date.now() + Math.random(),
+          name: file.name,
+          size: `${sizeInKB} KB`
+        }
+
+        this.uploadedFiles.push(fileInfo);
+      })
+
+      console.log(this.uploadedFiles);
+    }
+  }
+
+  removeFile(id: number) {
+    this.uploadedFiles = this.uploadedFiles.filter(file => file.id !== id);
+    console.log(this.uploadedFiles);
+  }
+
+  removeAllFiles() {
+    this.uploadedFiles = [];
+  }
+
+  openFile() {
+    
+  }
 
 }
